19dc3aea9bf42abf339743d95f5fa53bb47ae604
Gabriel dos Santos <133364553+Serafim02@users.noreply.github.com>
2023-08-10 16:35:59 -0300

    Add files via upload

---
+++ SORTEIO/aula.js
@@ -0,0 +1,17 @@
+//math.random() gera número 
+console.log(Math.random());
+console.log(Math.random() * 20);
+console.log(Math.random(7 - 3 + 1) +3 );
+
+//arredonda para o valor inteiro mais proximo 
+console.log(math.round (3.1));
+console.log(math.round (3.9));
+
+//arredonda para baixo 
+console.log(math.floor (3.1));
+console.log(math.floor (3.9));
+
+//arredonda para cima
+
+console.log(math.ceil (3.1));
+console.log(math.ceil (3.9));
